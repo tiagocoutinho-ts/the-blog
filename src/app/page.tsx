6136f07771d2b3a215d1a2aed02aaa310cb6f63d
Tiago Coutinho <tiagocoutinhots@gmail.com>
2025-09-19 17:22:33 -0300

    Concluido o CSS do FeaturedPost com Tailwind CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,14 +15,24 @@ export default async function HomePage() {
         <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
           <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
             <Image 
-            className="group-hover:scale-105 transition"
+            className="w-full h-full object-cover object-center group-hover:scale-105 transition"
             src="/images/bryen_0.png" 
             width={1200} 
             height={720} 
-            alt="Título do Post"></Image>
+            alt="Título do Post"
+            priority></Image>
           </Link>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati porro voluptatem magnam dolores ducimus facere temporibus non, tenetur accusantium corrupti repellat reprehenderit eveniet minus totam, quia veritatis fugit autem necessitatibus?
+
+          <div className="flex flex-col gap-4 sm: justify-center">
+            <time 
+            className="text-slate-600 text-sm/tight block "
+            dateTime="2025-09-19">20/04/2025 05:20</time>
+            
+            <h1 className="text-2xl/tight mb-4 font-extrabold sm:text-4xl">
+              <Link href="#">Lorem ipsum dolor sit amet.</Link>
+            </h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati porro voluptatem magnam dolores ducimus facere temporibus non, tenetur accusantium corrupti repellat reprehenderit eveniet minus totam, quia veritatis fugit autem necessitatibus?</p>
           </div>
         </section>
 
