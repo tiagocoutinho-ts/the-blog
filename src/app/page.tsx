e8c08ca84d3b430f7d3f65acd8129ab14f6d1d33
Tiago Coutinho <tiagocoutinhots@gmail.com>
2025-09-21 17:05:17 -0300

    Criado o componente PostImageCover com Link + Image do Next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,47 +3,50 @@ import { Header } from "@/components/Header"
 import { Suspense } from "react"
 import { SpinLoader } from "@/components/SpinLoader"
 import PostsList from "@/components/PostsList"
-import Image from "next/image"
-import Link from "next/link"
 import { PostHeading } from "@/components/PostHeading"
+import { PostCoverImage } from "@/components/PostCoverImage"
 
 export default async function HomePage() {
 
   return (
-      <Container>
-        <Header/>
+    <Container>
+      <Header />
 
-        <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
-          <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
-            <Image 
-            className="w-full h-full object-cover object-center group-hover:scale-105 transition"
-            src="/images/bryen_0.png" 
-            width={1200} 
-            height={720} 
-            alt="Título do Post"
-            priority></Image>
-          </Link>
+      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
+        <PostCoverImage
+          linkProps={{
+            href: "/post/exemple"
+          }}
 
-          <div className="flex flex-col gap-4 sm: justify-center">
-            <time 
+          imageProps={{
+            width: 1200,
+            height: 720,
+            src: "/images/bryen_9.png",
+            alt: "Alt da imagem",
+            priority: true
+          }}
+        />
+
+        <div className="flex flex-col gap-4 sm: justify-center">
+          <time
             className="text-slate-600 text-sm/tight block "
             dateTime="2025-09-19">20/04/2025 05:20</time>
 
-            <PostHeading as="h1" url="#">
-              Lorem ipsum dolor sit amet consectetur.
-            </PostHeading>
+          <PostHeading as="h1" url="#">
+            Lorem ipsum dolor sit amet consectetur.
+          </PostHeading>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati porro voluptatem magnam dolores ducimus facere temporibus non, tenetur accusantium corrupti repellat reprehenderit eveniet minus totam, quia veritatis fugit autem necessitatibus?</p>
-          </div>
-        </section>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati porro voluptatem magnam dolores ducimus facere temporibus non, tenetur accusantium corrupti repellat reprehenderit eveniet minus totam, quia veritatis fugit autem necessitatibus?</p>
+        </div>
+      </section>
 
-        <Suspense fallback={<SpinLoader />}>
-          <PostsList />
-        </Suspense>
-        
-        <footer className="text-center">
-          <p>Footer</p>
-        </footer>
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
+
+      <footer className="text-center">
+        <p>Footer</p>
+      </footer>
     </Container>
   )
 }
