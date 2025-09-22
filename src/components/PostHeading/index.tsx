5b4dd1b40501fd18afe1321365f97fbb4a318861
Tiago Coutinho <tiagocoutinhots@gmail.com>
2025-09-22 17:29:57 -0300

    Criado a Grid responsiva de PostList com Tailwind CSS

--- src/components/PostHeading/index.tsx
+++ src/components/PostHeading/index.tsx
@@ -8,15 +8,13 @@ type PostHeadingProps = {
 
 export function PostHeading({ children, url, as: Tag = "h1"}: PostHeadingProps) {
     const headingClassesMap = {
-        h1: "text-2xl/tight mb-4 sm:text-4xl",
-        h2: "text-2xl/tight bg-red-500 mb-4 sm:text-4xl"
+        h1: "text-2xl/tight mb-4 sm:text-4xl font-extrabold",
+        h2: "text-2xl/tight font-bold"
     }
 
-    const commonClasses = "font-extrabold"
-
     return (
-        <Tag className={`${headingClassesMap[Tag]} ${commonClasses}`}>
-            <Link href={url}>{children}</Link>
+        <Tag className={headingClassesMap[Tag]}>
+            <Link className="hover:text-slate-600 transition" href={url}>{children}</Link>
         </Tag>
     )
 }
