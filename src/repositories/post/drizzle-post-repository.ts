e1d1f215f806948abdb8fd70033dfeb49f0c489a
Tiago Coutinho <tiagocoutinhots@gmail.com>
2025-10-03 17:07:47 -0300

    Continuando a criação do repositório DrizzlePostRepository

--- src/repositories/post/drizzle-post-repository.ts
+++ src/repositories/post/drizzle-post-repository.ts
@@ -1,35 +1,51 @@
 import { PostModel } from "@/models/post/post-model";
 import { PostRepository } from "./repository";
 import { drizzleDb } from "@/db/drizzle";
-import { postsTable } from "@/db/drizzle/schemas";
-import { desc, eq } from "drizzle-orm";
 
 export class DrizzlePostRepository implements PostRepository {
   async findAllPublic(): Promise<PostModel[]> {
     const posts = await drizzleDb.query.posts.findMany({
-      orderBy: (posts, {desc}) => desc(posts.createdAt),
-      where: (posts, {eq}) => eq(posts.published, true)
+      orderBy: (posts, { desc }) => desc(posts.createdAt),
+      where: (posts, { eq }) => eq(posts.published, true)
     })
+
+    console.log("/findAllPublic")
 
     return posts
   }
 
   async findBySlugPublic(slug: string): Promise<PostModel> {
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { eq, and }) =>
+        and(eq(posts.published, true), eq(posts.slug, slug)),
+    })
 
+    if (!post) throw new Error("Post não encontrado para slug")
+
+    return post
   }
-
   async findAll(): Promise<PostModel[]> {
+    const posts = await drizzleDb.query.posts.findMany({
+      orderBy: (posts, { desc }) => desc(posts.createdAt),
+    })
 
+    return posts
   }
-
+  
   async findById(id: string): Promise<PostModel> {
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { eq }) => eq(posts.id, id),
+    });
 
+    if (!post) throw new Error('Post não encontrado para ID');
+
+    return post;
   }
 }
 
-(async () => {
-  const repo = new DrizzlePostRepository()
-  const posts = await repo.findAllPublic()
+// (async () => {
+//   const repo = new DrizzlePostRepository()
+//   const posts = await repo.findById("  ")
 
-  posts.forEach(post => console.log(post.slug))
-})()
+//   console.log(posts)
+// })()
