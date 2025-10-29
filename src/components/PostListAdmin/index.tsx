5ab73e97a0c991f9143a2bc08b09f2ea931b3422
Tiago Coutinho <tiagocoutinhots@gmail.com>
2025-10-29 17:27:00 -0300

    Dialog: adicionando as funcionalidades do componente

--- src/components/PostListAdmin/index.tsx
+++ src/components/PostListAdmin/index.tsx
@@ -20,41 +20,7 @@ export default async function PostListAdmin() {
         </div>
       })}
 
-      <div className={clsx(
-        "fixed z-50 top-0 inset-0 bg-black/50 backdrop-blur-xs",
-        "flex items-center justify-center"
-      )}>
-
-        <div className={clsx(
-          "bg-slate-100 p-6 rounded-lg max-w-2xl mx-6",
-          "flex flex-col gap-6",
-          "shadow-lg shadow-black/30 text-center"
-        )}>
-
-          <h3 className="text-xl font-extrabold">Título do diálogo</h3>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus quo itaque quam pariatur, minima, eum totam similique, vel tempora veritatis accusantium necessitatibus quia magnam quod ipsa enim voluptates ea dolores.</p>
-
-          <div className="flex items-center justify-around">
-            <button className={clsx(
-              "bg-slate-200 hover:bg-slate-300 transition text-slate-950",
-              "flex items-center justify-center",
-              "py-2 px-4 rounded-lg cursor-pointer"
-            )}
-            autoFocus
-            >
-              Cancelar</button>
-
-            <button className={clsx(
-              "bg-blue-500 hover:bg-blue-600 transition text-blue-50",
-              "flex items-center justify-center",
-              "py-2 px-4 rounded-lg cursor-pointer"
-            )}>
-
-              Ok</button>
-          </div>
-
-        </div>
-      </div>
+      
     </div>
   )
 }
--- src/components/admin/DeletePostButton/index.tsx
+++ src/components/admin/DeletePostButton/index.tsx
@@ -1,9 +1,10 @@
 "use client"
 
 import { deletePostAction } from "@/actions/post/delete-post-action";
+import { Dialog } from "@/components/Dialog";
 import clsx from "clsx";
 import { Trash2Icon } from "lucide-react";
-import { useTransition } from "react";
+import { useState, useTransition } from "react";
 
 type DeletePostButtonProps = {
   id: string,
@@ -12,31 +13,48 @@ type DeletePostButtonProps = {
 
 export function DeletePostButton({ id, title }: DeletePostButtonProps) {
   const [isPeding, startTransition] = useTransition()
+  const [showDialog, setShowDialog] = useState(false)
 
   function handleClick() {
-    if(!confirm("Tem certeza que deseja realizar essa ação?")) return
+    setShowDialog(true)
+  }
+
+  function handleConfirm() {
+    if (!confirm("Tem certeza que deseja realizar essa ação?")) return
 
     startTransition(async () => {
       const result = await deletePostAction(id)
       alert(`O result é: ${result}`)
     })
 
+    setShowDialog(false)
   }
 
   return (
-    <button className={clsx(
-      "text-red-500 cursor-pointer transition ",
-      "[&_svg]:w-4 [&_svg]:h-4",
-      "hover:scale-120 hover:text-red-700",
-      "disabled:text-slate-600 disabled:cursor-not-allowed"
-    )}
+    <>
+      <button className={clsx(
+        "text-red-500 cursor-pointer transition ",
+        "[&_svg]:w-4 [&_svg]:h-4",
+        "hover:scale-120 hover:text-red-700",
+        "disabled:text-slate-600 disabled:cursor-not-allowed"
+      )}
 
-      aria-label={`Apagar post: ${title}`}
-      title={`Apagar post: ${title}`}
-      onClick={handleClick}
-      disabled={isPeding}>
-      <Trash2Icon />
+        aria-label={`Apagar post: ${title}`}
+        title={`Apagar post: ${title}`}
+        onClick={handleClick}
+        disabled={isPeding}>
+        <Trash2Icon />
 
-    </button>
+      </button>
+      {showDialog && (
+        <Dialog
+          isVisible={showDialog}
+          title="Apagar post?"
+          content={`Tem certeza que deseja apagar o post: ${title}`}
+          onCancel={() => setShowDialog(false)}
+          onConfirm={handleConfirm}
+          disabled={isPeding}
+        />)}
+    </>
   )
 }
